986dae524766b2b2d9118f3d36b5655fe210efee
nieuvi <viyen482@gmail.com>
2023-11-12 13:56:36 +0700

    third

---
+++ htmlReportGenerator.js
@@ -0,0 +1,22 @@
+const reporter = require('cucumber-html-reporter');
+const cucumber = require('./cucumber');
+var date = new Date();
+var currentDate = date.getDate() + '_' + (date.getMonth()+1) + '_' + date.getFullYear() + '_' + date.getHours() + '_' + date.getMinutes() + '_' + date.getSeconds() + '_' + date.getMilliseconds();
+var options = {
+    brandTitle: "demo test scenarios",
+    theme: 'bootstrap',
+    jsonFile: 'Reports/cucumber_report.json',
+    output: 'Reports/cucumber_report_' + currentDate + '.html',
+    sceenshotsDirectory: './Screenshots',
+    storeScreenshots: true,
+    reportSuiteAsScenarios: true,
+    lanchReport: true,
+    metadata: {
+        "App Version":"1.1.1",
+        "Test Environment":"QA",
+        "Platform":"Web/Angular",
+        "Sprint":"001"
+    }
+};
+
+reporter.generate(options);
