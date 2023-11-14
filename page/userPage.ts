7907b423df34f95f88cb9e66ee4a60cd6bfa3983
nnyvi <nieuvi482@gmail.com>
2023-11-14 16:54:12 +0700

    add userPage file

--- page/userPage.ts
+++ page/userPage.ts
@@ -1,3 +1,35 @@
 export class UserPage {
-    
+    private baseURL = 'https://fakestoreapi.com/users';
+    private response : Response;
+
+    async postRequest () {
+        this.response = await fetch(this.baseURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                email:'John@gmail.com',
+                    username:'johnd',
+                    password:'m38rmF$',
+                    name:{
+                        firstname:'John',
+                        lastname:'Doe'
+                    },
+                    address:{
+                        city:'kilcoole',
+                        street:'7835 new road',
+                        number:3,
+                        zipcode:'12926-3874',
+                        geolocation:{
+                            lat:'-37.3159',
+                            long:'81.1496'
+                        }
+                    },
+                    phone:'1-570-236-7033'
+            })
+        })
+        const responseBody = await this.response.json();
+        console.log(responseBody);
+    }
 }
