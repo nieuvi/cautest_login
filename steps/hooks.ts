46f34336fc11b734609d9b3304c99ebab95ec008
nnyvi <nieuvi482@gmail.com>
2023-11-16 14:07:28 +0700

    env & browsers

--- steps/hooks.ts
+++ steps/hooks.ts
@@ -1,24 +1,38 @@
 import { After, Before, setDefaultTimeout } from '@cucumber/cucumber';
-import { Browser, chromium, Page } from 'playwright';
+import { Browser, chromium, firefox, Page, webkit } from 'playwright';
+import * as dotenv from 'dotenv'
 
 let page: Page;
 let browser: Browser;
 
+dotenv.config({
+    override: true,
+    path: `./helper/env/.env.browser`
+})
+
 setDefaultTimeout(60000*2);
 
 Before(async () => {
-    try {
-        browser = await chromium.launch({ headless: false });
+    const browserType = process.env.BROWSER;
+    switch (browserType) {
+        case 'chromium':
+            browser = await chromium.launch({ headless: false });
+        break;
+        case 'webkit':
+            browser = await webkit.launch({ headless: false });
+        break;
+        case 'firefox':
+            browser = await firefox.launch({ headless: false });
+        break;
+        default: 
+            console.log ('No browser is required');
+    }
         const context = await browser.newContext();
         page = await context.newPage();
         await page.goto("https://qa.givenow.com.au/login");
         console.log(`captured site title as ${await page.title()}`);
-    }
-    catch(error) {
-        console.log(`chrome navigation to demo site failed due to ${error}`)
-        throw new Error(`chrome navigation to demo site failed due to ${error}`);
-    }
-    return page;
+        console.log("run on " + browserType)
+        return page;
 });
 
 After(async () => {
